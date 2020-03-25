9d1207b08bd6e5abf123a8832bed060b43c155cd
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 20:54:34 +0300

    Initial commit

---
+++ Struct App/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_App
+{
+    class Program
+    {
+        struct PersonelBilgisi
+        {
+            public string PersonelID; // 12344535342
+            public string PersonelAd;
+            public string PersonelSoyadı;
+            public string Şifre;
+        }
+
+        static void Main(string[] args)
+        {
+            PersonelBilgisi PB = new PersonelBilgisi();
+
+            PB.PersonelID = "12345";
+            PB.PersonelAd = "Murat";
+            PB.PersonelSoyadı = "Bilginer";
+            PB.Şifre = "12345";
+
+            Console.WriteLine(PB.PersonelID);
+
+            Console.ReadKey();
+        }
+    }
+}
